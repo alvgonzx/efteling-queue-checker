d92526aeb9635c35bb692fbcb61ea7b4099c67af
Álvaro González <alvgonz.ojeda@gmail.com>
2023-07-02 13:01:50 +0200

    [dev] created cards for rides

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,15 @@
 <script>
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
+  import Card from './lib/Card.svelte';
 </script>
 
 <main>
-  <h1 class="text-5xl">hello world</h1>
+  <h1 class="text-3xl text-center font-bold pt-2">Efteling Queue Checker</h1>
+		<div class="py-6 flex flex-col gap-4 w-full flex-nowrap md:flex-wrap md:flex-row">
+      <Card name="Baron 1898" queue="70"></Card>
+      <Card name="Baron 1898" queue="20"></Card>
+      <Card name="Baron 1898" queue="10"></Card>
+      <Card name="Baron 1898" queue="30"></Card>
+      <Card name="Baron 1898" queue="60"></Card>
+      <Card name="Baron 1898" queue="40"></Card>
+    </div>
 </main>
---
+++ src/lib/Card.svelte
@@ -0,0 +1,28 @@
+<script>
+	export let name, queue;
+</script>
+
+<div class="md:w-1/4 block max-w-sm mx-auto  w-3/4 p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+    <h5 class="mb-2 text-center text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{name}</h5>
+
+    {#if queue > 30}
+    <div class="w-full flex justify-center">
+        <div class="text-xs inline-flex items-center font-bold leading-sm uppercase px-3 py-1 bg-red-200 text-red-700 rounded-full">
+            {queue} min
+        </div>
+    </div>
+
+    {:else if queue == 30}
+    <div class="w-full flex justify-center">
+        <div class="text-xs inline-flex items-center font-bold leading-sm uppercase px-3 py-1 bg-orange-200 text-orange-700 rounded-full">
+            {queue} min
+        </div>
+    </div>
+    {:else}
+    <div class="w-full flex justify-center">
+        <div class="text-xs inline-flex items-center font-bold leading-sm uppercase px-3 py-1 bg-green-200 text-green-700 rounded-full">
+            {queue} min
+        </div>
+    </div>
+    {/if}
+</div>
